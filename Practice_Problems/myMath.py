c2b2888b914094985b3bad10a32dda705298b263
David Percy <bjpd@pdx.edu>
2022-01-28 21:34:34 -0800

    updating curriculum

---
+++ Practice_Problems/myMath.py
@@ -0,0 +1,10 @@
+#import numpy as np
+def circleArea(myradius):
+    """
+    return the area of a circle, given a radius as input
+    """
+    if myradius is None:
+        pass
+    else:
+        return  np.pi * myradius**2
+        #return myarea  
